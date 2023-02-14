34e0a977676f1dec29178a3cbe5030aa86c9b84a
Chendada-8474 <raymond96383@gmail.com>
2023-02-14 15:02:13 +0800

    grass owl

--- utils/config.py
+++ utils/config.py
@@ -16,8 +16,8 @@ mel_specrogram_config = {
 training_config = {
     "epochs": 5,  # number of training epoch
     "learning_rate": 0.0005,
-    "batch_size": 16,
-    "dataset": "gogo-owl",  # traning dataset folder name
+    "batch_size": 32,
+    "dataset": "grassowl",  # traning dataset folder name
     "cpu_workers": cpu_count() // 2,
     "skip_false_rate": 0.6,  # The probability skip the training window if all annotations are 0
     "model_name": "grassowl",  # folder name of training outpout
